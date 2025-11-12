50582e4b39869ada1166699f597f2ecc41c8b179
Ayo <Ayoroq@gmail.com>
2025-11-11 23:58:12 -0500

    Added the option to shuffle and display arrays

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -1,33 +1,56 @@
+import React, { useState, useEffect } from "react";
+export default function DisplayImage({ imageData, loading }) {
+  const [clickedImagesId, setClickedImagesId] = useState([]);
+  const [alreadyClicked, setIsAlreadyClicked] = useState(false);
+  const [shuffledImages, setShuffledImages] = useState([]);
 
-export default function DisplayImage({imageData,loading}) {
-    // This is the Fisher-Yates shuffle.
-   function shuffleImage(imageData){
-    for (let i = imageData.length - 1; i > 0; i--) {
+  useEffect(() => {
+    if (imageData && imageData.length > 0) {
+      setShuffledImages(shuffleAndSlice([...imageData]));
+    }
+  }, [imageData]);
+
+  // this is the 
+  function shuffleAndSlice(data) {
+    const shuffled = [...data];
+    for (let i = shuffled.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [imageData[i], imageData[j]] = [imageData[j], imageData[i]];
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
-    return imageData.slice(0,10)
-   }
-   imageData = shuffleImage(imageData)
-    if (loading) {
-      return <div>Loading...</div>;
-    }
+    return shuffled.slice(0, 10);
+  }
 
-    if (!imageData) {
-      return  null;
+  function handleOnclick(event) {
+    const clickedImage = event.target;
+    if (clickedImagesId.includes(clickedImage.id)) {
+      setIsAlreadyClicked(true);
     }
+    setClickedImagesId((prevClickedImagesId) => {
+      return [...prevClickedImagesId, clickedImage.id];
+    });
+    setShuffledImages(shuffleAndSlice([...imageData]));
+  }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-      <ul className="image-container">
-        {imageData.map((image) => (
+  if (!imageData) {
+    return null;
+  }
+
+  return (
+    <ul className="image-container">
+      {shuffledImages.map((image) => (
         <li key={image.id} className="image-item">
           <img
+            id={image.id}
             src={image.urls.regular}
             alt={image.alt_description}
             className="image"
+            onClick={handleOnclick}
           />
         </li>
-        ))}
-      </ul>
-    );
-  }
+      ))}
+    </ul>
+  );
+}
