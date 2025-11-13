5b36fe45884137d38cf64515974e004dba95fe00
Ayo <Ayoroq@gmail.com>
2025-11-12 20:10:43 -0500

    Added a loading screen object

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,7 +1,10 @@
+import Lottie from "lottie-react";
+import loading from "../assets/lottie/loading.json";
 export default function LoadingScreen() {
     return (
         <div className="loading-screen">
             <p>This is the loading screen</p>
+            <Lottie animationData={loading} autoPlay={true} />
         </div>
     )
 }
