76a9e0e9429d4fa5db2c343a44e67d536bfdc17d
Ayo <Ayoroq@gmail.com>
2025-11-14 09:17:45 -0500

    Restructured the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,11 @@
-import ideaImage from "../assets/landing-page-assets/andy-idea.svg";
 export default function LandingPage() {
   return (
     <div className="landing-page">
-      <h1 className="name">PHOTOGRAPH <br/> RECALL</h1>
-      <h3 className="description"> A game to test your memory</h3>
-      <div className="landing-images">
-        <img src={ideaImage} alt="idea" className="idea-image" />
-      </div>
+      <h1 className="name">MEMORY</h1>
+      <p className="description"> A game to test your memory</p>
+      <p className="description">Select a theme and start playing!</p>
+
+      <h2>Click on an image to get a point. <br/>Click on the same image twice and you lose</h2>
     </div>
   );
 }
