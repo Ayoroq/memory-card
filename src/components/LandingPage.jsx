97336ec2ee9bd7dcb210c45962d5ea2d5a8624a7
Ayo <Ayoroq@gmail.com>
2025-11-14 01:11:11 -0500

    Added some stylings

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,12 @@
+import ideaImage from "../assets/landing-page-assets/andy-idea.svg";
 export default function LandingPage() {
-    return (
-        <div className="landing-page">
-            <h1>Welcome to the Landing Page</h1>
-            <p>This is the landing page content.</p>
-        </div>
-    )
+  return (
+    <div className="landing-page">
+      <h1 className="name">PHOTOGRAPH <br/> RECALL</h1>
+      <h3 className="description"> A game to test your memory</h3>
+      <div className="landing-images">
+        <img src={ideaImage} alt="idea" className="idea-image" />
+      </div>
+    </div>
+  );
 }
