21e5f3046917557bf1fb53227c104881442581ae
Ayo <Ayoroq@gmail.com>
2025-11-15 12:34:09 -0500

    Restructured the landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,19 @@
+import andy from '../assets/landing-page-assets/andy-idea.svg';
+import andy2 from '../assets/landing-page-assets/andy-break.svg';
+import andy3 from '../assets/landing-page-assets/andy-time.svg';
+
 export default function LandingPage() {
   return (
     <div className="landing-page">
       <h1 className="name">MEMORY</h1>
-      <p className="description"> A game to test your memory</p>
-      <p className="description">Select a theme and start playing!</p>
-
-      <h2>Click on an image to get a point. <br/>Click on the same image twice and you lose</h2>
+      <div className="button-container">
+        <button type="button" className="get-started">Get Started</button>
+      </div>
+      <div className="landing-images">
+        <img src={andy} alt="andy-idea" className='andy' />
+        <img src={andy2} alt="andy-break" className='andy' />
+        <img src={andy3} alt="andy-time" className='andy' />
+      </div>
     </div>
   );
 }
